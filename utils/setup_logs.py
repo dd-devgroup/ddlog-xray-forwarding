6ec42f0334d0db560725c24b66915e9aadc31e15
fxfuren <nikitostimoshenko@gmail.com>
2025-07-30 18:57:44 +0300

    Refactor get_local_ip function for clarity and remove duplicate definition

--- utils/setup_logs.py
+++ utils/setup_logs.py
@@ -6,6 +6,17 @@ import os
 import json
 import socket
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+    
 NODES_FILE = "nodes.json"
 
 CENTRAL_HOST = get_local_ip()
@@ -24,16 +35,7 @@ input(type="imfile"
 
 *.* @@{central_host}:{central_port}
 """
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-    except Exception:
-        ip = "127.0.0.1"
-    finally:
-        s.close()
-    return ip
+
 
 def load_nodes():
     if os.path.exists(NODES_FILE):
